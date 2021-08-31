4cf22f14baf1766d8346898c48e12cd90019a678
Julian Aguilar <julianaguilar98@gmail.com>
2021-08-30 21:57:12 -0500

    Initial commit

---
+++ client/src/components/ChannelListContainer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { ChannelList, useChatContext } from 'stream-chat-react';
+import Cookies from 'universal-cookie';
+
+import { ChannelSearch, TeamChannelList, TeamChannelPreview } from './';
+
+
+
+const ChannelListContainer = () => {
+    return (
+        <div>
+            ChannelListContainer
+        </div>
+    );
+}
+
+export default ChannelListContainer;
