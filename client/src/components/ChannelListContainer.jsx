f30314c936409afe009dcb2857647014565c6210
Julian Aguilar <julianaguilar98@gmail.com>
2021-08-30 22:19:51 -0500

    added assets folder, edited ChannelListContainer

--- client/src/components/ChannelListContainer.jsx
+++ client/src/components/ChannelListContainer.jsx
@@ -3,14 +3,29 @@ import { ChannelList, useChatContext } from 'stream-chat-react';
 import Cookies from 'universal-cookie';
 
 import { ChannelSearch, TeamChannelList, TeamChannelPreview } from './';
+import JulesIcon from '../assets/JulesLogo.png'
+import LogoutIcon from '../assets/logout.png'
 
-
+const SideBar = () => (
+    <div className="channel-list___sidebar">
+        <div className="channel-list__sidebar__icon1">
+            <div classname="icon1__inner">
+                <img src={JulesIcon} alt="Jules" width="30" />
+            </div>
+        </div>
+        <div className="channel-list__sidebar__icon2">
+            <div classname="icon1__inner">
+                <img src={LogoutIcon} alt="Logout" width="30" />
+            </div>
+        </div>
+    </div>
+)
 
 const ChannelListContainer = () => {
     return (
-        <div>
-            ChannelListContainer
-        </div>
+        <>
+            <Sidebar />
+        </>
     );
 }
 
