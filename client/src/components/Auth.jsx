315e35176982b9876514eb479e66e1c0da93f976
Julian Aguilar <julianaguilar98@gmail.com>
2021-09-05 22:46:09 -0500

    added Results Dropdown component

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -29,6 +29,7 @@ const Auth = () => {
         const { username, password, phoneNumber, avatarURL } = form;
 
         const URL = 'https://localhost:5000/auth';
+        // const URL = 'https://medical-pager.herokuapp.com/auth';
 
         const { data: { token, userId, hashedPassword, fullName } } = await axios.post(`${URL}/${isSignup ? 'signup' : 'login'}`, {
             username, password, fullName: form.fullName, phoneNumber, avatarURL,
@@ -150,4 +151,4 @@ const Auth = () => {
     )
 }
 
-export default Auth;
+export default Auth
