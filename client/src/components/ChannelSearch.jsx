dd05321e57bfe94e6d4202323ac96b8d1c9bfc47
Julian Aguilar <julianaguilar98@gmail.com>
2021-09-05 23:12:38 -0500

    added Twilio support for SMS notifications

--- client/src/components/ChannelSearch.jsx
+++ client/src/components/ChannelSearch.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useChatContext } from 'stream-chat-react';
 import { ResultsDropdown } from '.';
 
+import { ResultsDropdown  } from './';
 import { SearchIcon } from '../assets';
 
 const ChannelSearch = ({ setToggleContainer }) => {
